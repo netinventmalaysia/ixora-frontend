916018208b7e80bae1344128389b6585fb7217be
azimhamdan <azimhamdan90@gmail.com>
2025-09-27 11:24:28 +0800

    Reformatting chart

--- types/react-apexcharts.d.ts
+++ types/react-apexcharts.d.ts
@@ -1,15 +1,15 @@
 declare module 'react-apexcharts' {
-  import { ComponentType } from 'react';
-  import type { ApexOptions } from 'apexcharts';
+    import { ComponentType } from 'react';
+    import type { ApexOptions } from 'apexcharts';
 
-  export interface ReactApexChartProps {
-    type?: 'line' | 'area' | 'bar' | 'pie' | 'donut' | 'radialBar' | 'scatter' | 'bubble' | 'heatmap' | 'candlestick' | 'radar' | 'polarArea' | 'rangeBar' | 'treemap';
-    series: any;
-    options?: ApexOptions;
-    width?: string | number;
-    height?: string | number;
-  }
+    export interface ReactApexChartProps {
+        type?: 'line' | 'area' | 'bar' | 'pie' | 'donut' | 'radialBar' | 'scatter' | 'bubble' | 'heatmap' | 'candlestick' | 'radar' | 'polarArea' | 'rangeBar' | 'treemap';
+        series: any;
+        options?: ApexOptions;
+        width?: string | number;
+        height?: string | number;
+    }
 
-  const ReactApexChart: ComponentType<ReactApexChartProps>;
-  export default ReactApexChart;
+    const ReactApexChart: ComponentType<ReactApexChartProps>;
+    export default ReactApexChart;
 }
